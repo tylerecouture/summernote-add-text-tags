3c106847c2313191b0e3abec01261b554e7a003b
AgeOf <tylerecouture@gmail.com>
2017-09-04 23:21:47 -0700

    basic parsing across nodes closes #

--- summernote-add-text-tags.js
+++ summernote-add-text-tags.js
@@ -70,6 +70,42 @@
                 ]).render();
             });
 
+            self.areDifferentBlockElements = function(startEl, endEl) {
+                var startElDisplay = getComputedStyle(startEl, null).display;
+                var endElDisplay  = getComputedStyle(endEl, null).display;
+
+                if(startElDisplay !== 'inline' && endElDisplay !== 'inline') {
+                    console.log("Can't insert across two block elements.")
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            };
+
+            self.isSelectionParsable = function(startEl, endEl) {
+
+                if(startEl.isSameNode(endEl)) {
+                    return true;
+                }
+                if( self.areDifferentBlockElements(startEl, endEl)) {
+                    return false;
+                }
+                // if they're not different block elements, then we need to check if they share a common block ancestor
+                // could do this recursively, if we want to back farther up the node chain...
+                var startElParent = startEl.parentElement;
+                var endElParent = endEl.parentElement;
+                if( startEl.isSameNode(endElParent)
+                    || endEl.isSameNode(startElParent)
+                    || startElParent.isSameNode(endElParent) )
+                {
+                    return true;
+                }
+                else
+                    console.log("Unable to parse across so many nodes. Sorry!")
+                    return false;
+            };
+
             self.wrapInTag = function (tag) {
                 // from: https://github.com/summernote/summernote/pull/1919#issuecomment-304545919
                 // https://github.com/summernote/summernote/pull/1919#issuecomment-304707418
@@ -80,9 +116,26 @@
 
                     // Only wrap tag around selected text
                     if (selected.startOffset !== selected.endOffset) {
+
                         var range = selected.cloneRange();
-                        range.surroundContents(document.createElement(tag));
+
+                        var startParentElement = range.startContainer.parentElement;
+                        var endParentElement = range.endContainer.parentElement;
+
+                        // if the selection starts and ends different elements, we could be in trouble
+                        if( ! startParentElement.isSameNode(endParentElement)) {
+                            if ( ! self.isSelectionParsable(startParentElement, endParentElement)) {
+                                return;
+                            }
+                        }
+
+                        var newNode = document.createElement(tag);
+                        // https://developer.mozilla.org/en-US/docs/Web/API/Range/surroundContents
+                        // Parses inline nodes, but not block based nodes...blocks are handled above.
+                        newNode.appendChild(range.extractContents());
+                        range.insertNode(newNode)
                     }
+                    //context.invoke('editor.restoreRange');
                 }
             };
         }
